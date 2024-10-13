85aaba677ec2ca23105c0fb4e612f8855fde31bd
Mohsin Baloch <160867480+Mohsin181@users.noreply.github.com>
2024-10-13 19:19:57 +0500

    Initial commit

---
+++ app/footer/page.tsx
@@ -0,0 +1,4 @@
+export default function Footer () {
+    return (<h1>footer</h1>)
+ }
+ 
---
+++ app/home/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+
+export default function Home () {
+  return(
+
+
+
+
+
+    <div>
+    <ul ></ul>
+<div className="text-centre"><Link href="/about">about</Link></div>
+<div className="text-center bg-green-200 text-yellow-600"><Link href="/contact">contact</Link></div>
+<div className="text-center bg-blue-200 text-red-500"><Link href="/about">navbar</Link></div>
+<div className="text-center bg-yellow-200 "><Link href="/footer">footer</Link></div>
+
+  </div>
+
+
+
+
+  )
+}
